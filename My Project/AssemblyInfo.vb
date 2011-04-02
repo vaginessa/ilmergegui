dc1942f959b7ac2fa5c229395a5bd61dccf253f9
Igor Ramadas <igoramadas@users.noreply.github.com>
2011-04-02 21:55:38 +0200

    First source update towards version 2.0.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,14 @@
+Imports System
+Imports System.Reflection
+Imports System.Security
+Imports Devv.Core.Utils
+
+<Assembly: CLSCompliant(True)> 
+<Assembly: AssemblyTitle("ILMerge-GUI")> 
+<Assembly: AssemblyProduct("ILMerge-GUI")> 
+<Assembly: AssemblyVersion("2.0.*")> 
+<Assembly: AssemblyCompanyAttribute(AssemblyUtil.COMPANY)> 
+<Assembly: AssemblyCopyrightAttribute(AssemblyUtil.COPYRIGHT)> 
+<Assembly: AssemblyTrademarkAttribute(AssemblyUtil.TRADEMARK)> 
+
+<Assembly: AllowPartiallyTrustedCallersAttribute()> 
