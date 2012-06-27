2e507a46a826023d8c1ca6ed79eb282198df7b70
wvd_vegt <wvdvegt@users.noreply.github.com>
2012-06-27 18:25:22 +0200

    1) Rewrote in C# 2) Added Drag&Drop Support (files and directories) 3) Search for .NET Frameworks 4) Search for ILMerge.exe 5) x64 support 6) .NET 4.0 support 7) Save and loading of project settings 8) Automatic suggestion of primary assembly 9) Bugfixes. 10)Commandline support (filename and /merge switch)

--- IlMerge-Gui/My Project/AssemblyInfo.vb
+++ IlMerge-Gui/My Project/AssemblyInfo.vb
@@ -1,14 +1,13 @@
 Imports System
 Imports System.Reflection
 Imports System.Security
-Imports Devv.Core.Utils
 
 <Assembly: CLSCompliant(True)> 
 <Assembly: AssemblyTitle("ILMerge-GUI")> 
 <Assembly: AssemblyProduct("ILMerge-GUI")> 
 <Assembly: AssemblyVersion("2.0.*")> 
-<Assembly: AssemblyCompanyAttribute(AssemblyUtil.COMPANY)> 
-<Assembly: AssemblyCopyrightAttribute(AssemblyUtil.COPYRIGHT)> 
-<Assembly: AssemblyTrademarkAttribute(AssemblyUtil.TRADEMARK)> 
+<Assembly: AssemblyCompanyAttribute("Devv")> 
+<Assembly: AssemblyCopyrightAttribute("Devv")> 
+<Assembly: AssemblyTrademarkAttribute("Devv")> 
 
 <Assembly: AllowPartiallyTrustedCallersAttribute()> 
